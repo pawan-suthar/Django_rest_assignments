7dcdb09e8a9aab738c343779b0355ebe6858f4c5
Pawansuthar <pawansuthar1537209702362713@gmail.com>
2023-07-24 15:48:40 +0530

    use listmodel and create model in mixins

--- CurseApiCBV/app/views.py
+++ CurseApiCBV/app/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from .models import Course, CourseSerializer
 from rest_framework import status
 from django.http import Http404
+
 class CourseListView(APIView):
     def get(self, request):
         courses = Course.objects.all()
@@ -14,18 +15,22 @@ class CourseListView(APIView):
         ser = CourseSerializer(data = request.data)
         if ser.is_valid(): #if valid
             ser.save() #save data in python obj
-            return Response(ser.data, status=status.HTTP_201_CREATED) #return serliazied data
+            return Response(ser.data, status=status.HTTP_201_CREATED) 
         return Response(ser.errors)
+    
 class CourseDetailView(APIView):
+
     def get_course(self,pk):
         try:
             return Course.objects.get(pk=pk)
         except Course.DoesNotExist:
             raise Http404
+        
     def get(self,request,pk):
         courses = self.get_course(pk)
         ser  = CourseSerializer(courses)
         return Response(ser.data)
+    
     def put(self,request,pk):
         course = self.get_course(pk)
         ser = CourseSerializer(course,data = request.data)
@@ -33,6 +38,7 @@ class CourseDetailView(APIView):
             ser.save() #save data in python obj
             return Response(ser.data) #return serliazied data
         return Response(ser.errors)
+    
     def delete(self,request,pk):
         self.get_course(pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
