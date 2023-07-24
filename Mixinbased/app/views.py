6a42b4847f7c7fb0b53a2888b863d1011c057c0e
Pawansuthar <pawansuthar1537209702362713@gmail.com>
2023-07-24 16:58:31 +0530

    all done in mixins

--- Mixinbased/app/views.py
+++ Mixinbased/app/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render
 from requests import request
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Course, CourseSerializer
-from rest_framework import status
-from django.http import Http404
 from rest_framework import mixins, generics #used to get handler methods
-
-
-
-
 class CourseListView(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
     def get(self,request):
         return self.list(request)
     def post(self,request):
         return self.create(request)
 
-    
+class CourseDetailView(mixins.RetrieveModelMixin,mixins.DestroyModelMixin,mixins.UpdateModelMixin,generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self,request,pk):
+        return self.retrieve(request,pk)
+    def put(self,request,pk):
+        return self.update(request,pk)
+    def delete(self,request,pk):
+        return self.destroy(request,pk)
 
 
-class CourseDetailView(mixins.RetrieveModelMixin,generics.GenericAPIView):
-    pass
 
 
 
 
 
-
+# using apiview base class
 
 # class CourseListView(APIView):
 #     def get(self, request):
