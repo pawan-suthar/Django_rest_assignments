0db2a0e1c444b10b95edf7bb43589cd4c8dc3f72
Pawansuthar <pawansuthar1537209702362713@gmail.com>
2023-07-26 17:15:10 +0530

    hyperlinkserilizers

---
+++ Hyperlinkserlizers/app/serilizers.py
@@ -0,0 +1,26 @@
+# Serilizers
+
+from.models import * 
+from rest_framework import serializers
+
+# class CourseSer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Course
+#         fields = "__all__"
+
+# class InstructorSer(serializers.ModelSerializer):
+#     courses = CourseSer(many=True, read_only=True)
+#     class Meta:
+#         model = Instructor
+#         fields = "__all__"
+
+class CourseSer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+class InstructorSer(serializers.HyperlinkedModelSerializer):
+    courses = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name="course-detail")
+    class Meta:
+        model = Instructor
+        fields = ["name", "courses"]
