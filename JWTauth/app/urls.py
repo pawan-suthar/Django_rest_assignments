02d286d1ccd9accf6d485bc9be2ced8426ac41a1
Pawansuthar <pawansuthar1537209702362713@gmail.com>
2023-07-27 15:49:45 +0530

    ssadasd

--- JWTauth/app/urls.py
+++ JWTauth/app/urls.py
@@ -2,9 +2,17 @@
 from django.contrib import admin
 from django.urls import path
 from .views import * 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('instructors',instructorlistview.as_view(),name="instructorlistview"),
     path('courses',courseslistview.as_view(),name="courseslistview"),
     path('courses/<int:pk>',coursedetailview.as_view(),name="course-detail"),
     path('instructors/<int:pk>',instructordetailview.as_view(),name="instructor-detail"),
+    path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
